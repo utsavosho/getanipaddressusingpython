6737f3d81725434d5634c5da788173232ae385dc
Utsav Pandey <95593979+utsavosho@users.noreply.github.com>
2022-01-16 21:48:17 +0100

    Add files via upload

---
+++ getanipaddress.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import bs4
+import requests
+res = requests.get('https://www.myip.com/')
+
+res.text
+
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+soup
+
+ip = soup.find_all('span', {'id': 'ip'})
+ip[0].getText()
+my_ip = ip[0].getText()
+print(my_ip)
+
+
+# In[ ]:
+
+
+
+
